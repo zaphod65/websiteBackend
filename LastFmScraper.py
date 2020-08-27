0460b0703199457818213db32394968e76fa36b2
Douglas Graham <douglas.graham@sonymusic.com>
2020-08-27 13:31:34 +0100

    Update last fm scraper.

--- LastFmScraper.py
+++ LastFmScraper.py
@@ -5,34 +5,11 @@ from lxml import etree
 def scrape_from_string(content):
     tree = html.fromstring(content)
 
-    track_names = tree.xpath('//section[@id="recent-tracks-section"]/table//td[@class="chartlist-name"]//a[@class="link-block-target"]')
-    time_cells = tree.xpath('//td[@class="chartlist-timestamp"]/span[@title]')
+    track_names = tree.xpath('//section[@id="recent-tracks-section"]/table//td[@class="chartlist-name"]//a')
+    track_artists = tree.xpath('//section[@id="recent-tracks-section"]/table//td[@class="chartlist-artist"]//a')
+    track_times = tree.xpath('//section[@id="recent-tracks-section"]/table//td[contains(concat(" ",normalize-space(@class)," ")," chartlist-timestamp ")]//span')
 
-    tracks = []
-    for i, track in enumerate(track_names):
-        track_info = list(map(str.strip, track.get('title').split(u'\u2014')))
-        is_playing = False
-        if 'now-scrobbling' in track.getparent().getparent().getparent().get('class'):
-            is_playing = True
-        else:
-            # slight hack:
-            # if there is no "now playing" track then we get a different
-            # length time_cells list to the track_names list
-            # If they are the same length, then we can use the same
-            # index for both track names and play times
-            if len(time_cells) == len(track_names):
-                index = i
-            else:
-                # Here we have a "now playing" track and need to adjust for
-                # the shorter list of time cells
-                index = i - 1
-        tracks.append({
-            "artist": track_info[0],
-            "title": track_info[1],
-            "playing": is_playing,
-            "time": time_cells[index].get('title')
-        })
-    return tracks
+    return [{"title": x.text_content(), "artist": y.text_content(), "time": z.get('title')} for (x, y, z) in zip(track_names, track_artists, track_times)]
 
 
 if __name__ == '__main__':
